d9cdd5f2d009be1156fc74e2c22d2e979021d543
David BOHORQUEZ <davidb@David-BOHORQUEZ.local>
2025-02-17 11:24:10 +0100

    Ajout des tests unitaires pour les fonctions get_hardware_info

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Environnements_de_Tests
 
-## Documenation
+## Documentation
 - `test_osdetectory.py`
 	- 'mocker' est une "fixure" fournie par `pytest-mock`, qui va nous permettre de créer des "mocks" (des objets simulés) pour remplacer des parties du code réel pendant les tests.
 		- `def test_obtenir_sys_info(mocker):`
@@ -11,3 +11,8 @@
 			- On appelle la vrai fonction `obtenir_sys_info` du module `os_detector`.
 			- Grâce à `mocker.patch`, à chaque fois que cette fonction appellera `platform_uname`, elle utilisera notre version mockée avec les valeur simulées.
 			- On vérifie ensuite que le résultat de la fonction est bien égal à ce à quoi on s'attend (c'est-à-dire le dictionnaire `exprected_result`).
+	- Meme principe pour ce test, mais cette foi on teste la fonction `get_windows_hardware_info`.
+	- On va remplacer une autre fonction : `subprocess.check_output`, qui est utilisée pour exécuter des commndes dans le terminal et obtenir des informations sur le matériel.
+		- `def test_get_windows_hardware_info(mocker):`
+			- On remplace temporairement `subprocess.check_output` par un version simulée.
+			- `side_effect` permet de définir plusieurs réponses simulées, dans l'ordre.
--- test_osdetectory.py
+++ test_osdetectory.py
@@ -1,6 +1,8 @@
 import pytest
 import os_detector
 
+import sys
+
 def test_obtenir_sys_info(mocker):
     mock_uname = mocker.patch('platform.uname')
 
@@ -23,4 +25,54 @@ def test_obtenir_sys_info(mocker):
     }
 
     result = os_detector.obtenir_sys_info()
+    assert result == expected_result
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Le test ne fonctionne que sur Windows")
+def test_get_windows_hardware_info(mocker):
+    mock_check_output = mocker.patch('os_detector.subprocess.check_output')
+
+    mock_check_output.side_effect = [
+        "Name\nIntel(R) Core(TM) i7-8565U CPU @ 1.80GHz\n".encode(), # Simule la sortie de la commande  pour le CPU
+        "Capacity\n17179869184\n".encode()  # Simule la sortie de la commande pour la RAM (16 Go)
+    ]
+
+    expected_result = {
+        'CPU': 'Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz',
+        'Total Memory (GB)': 16.0 # 16 Go de RAM (17179869184 octets = 16 Go)        
+    }
+
+    result = os_detector.get_windows_hardware_info()
+    assert result == expected_result
+@pytest.mark.skipif(sys.platform != "linux", reason="Le test ne fonctionne que sur Linux")
+def test_get_linux_hardware_info(mocker):
+    mock_check_output = mocker.patch('os_detector.subprocess.check_output')
+
+    mock_check_output.side_effect = [
+        "Model name: Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz\n".encode(), # Sortie pour le CPU
+        "MemTotal: 16777216 kB\n".encode() # Sortie pour la RAM (16 Go)
+    ]
+
+    expected_result = {
+        'CPU': 'Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz',
+        'Total Memory (GB)': 16.0
+    }
+
+    result = os_detector.get_linux_hardware_info()
+    assert result == expected_result
+
+@pytest.mark.skipif(sys.platform != "darwin", reason="Le test ne fonctionne que sur macOS")
+def test_get_macOS_hardware_info(mocker):
+    mock_check_output = mocker.patch('os_detector.subprocess.check_output')
+
+    mock_check_output.side_effect = [
+        "Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz\n".encode(), # Sortie pour le CPU
+        "17179869184\n".encode() # Sortie pour la RAM (16 Go)
+    ]
+
+    expected_result = {
+        'CPU': 'Intel(R) Core(TM) i7-8565U CPU @ 1.80GHz',
+        'Total Memory (GB)': 16.0
+    }
+
+    result = os_detector.get_macOS_hardware_info()
     assert result == expected_result
